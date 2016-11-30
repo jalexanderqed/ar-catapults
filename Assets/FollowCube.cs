b4249e58c5aea3906188e51b98266e3c3cb2bd2a
John T. Alexander <jalexander.qed@gmail.com>
2016-11-29 17:11:39 -0800

    Initial commit with working shared space.

---
+++ Assets/FollowCube.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class FollowCube : NetworkBehaviour {
+
+    GameObject camera;
+
+	// Use this for initialization
+	void Start () {
+        if (!isLocalPlayer) return;
+        camera = GameObject.Find("SceneCamera");
+        this.transform.parent = camera.transform;
+        this.transform.localPosition = new Vector3(0, 0, 0);
+        this.transform.localRotation = Quaternion.identity;
+        this.transform.localScale = new Vector3(1, 1, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
