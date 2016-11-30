31a1e094a80dfe9e86dd1696221b3dd5698182bf
Jahwffrey <johnthomason@umail.ucsb.edu>
2016-11-30 10:18:59 -0800

    Added GPS reading

---
+++ Assets/GPSScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GPSScript : MonoBehaviour {
+
+	public GameObject text;
+	private TextMesh txt;
+	private bool locEnabled = true;
+
+	IEnumerator Wait(int secs){
+		yield return new WaitForSeconds(secs);
+	}
+
+	void Start () {
+		txt = text.GetComponent<TextMesh>();
+		if (!Input.location.isEnabledByUser) {
+			txt.text = "Please enable location services!";
+			locEnabled = false;
+		} else {
+			Input.location.Start (5f,1f);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (locEnabled) {
+			if (Input.location.status == LocationServiceStatus.Initializing) {
+				txt.text = "Initializing";
+			} else if (Input.location.status == LocationServiceStatus.Failed) {
+				txt.text = "Unable to determine device location";
+				return;
+			} else {
+				txt.text = "Latitude: " + Input.location.lastData.latitude + "\nLongitude: " + Input.location.lastData.longitude + "\nAltitude: " + Input.location.lastData.altitude + "\nAccuracy: " + Input.location.lastData.horizontalAccuracy + "\nTime: " + Input.location.lastData.timestamp;
+			}
+		}
+	}
+}
