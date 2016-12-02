a3b844f38dc6a8466dd0f4005ba15d8ee408d633
John T. Alexander <jalexander.qed@gmail.com>
2016-12-02 11:01:19 -0800

    Created pill guy

---
+++ Assets/PillGuyMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PillGuyMovement : MonoBehaviour {
+
+    public GameObject myTablet;
+
+	// Use this for initialization
+	void Start () {
+        	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = new Vector3(myTablet.transform.position.x, 2, myTablet.transform.position.z);
+	}
+}
---
+++ Assets/SpawnPillGuy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPillGuy : MonoBehaviour {
+
+    public GameObject pillGuyObject;
+
+	// Use this for initialization
+	void Start () {
+        GameObject pillGuy = Instantiate(pillGuyObject);
+        pillGuy.GetComponent<PillGuyMovement>().myTablet = this.transform.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
