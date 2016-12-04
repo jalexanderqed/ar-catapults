37d06eaf23564cb548196493f37ee7aac7857323
Jahwffrey <johnthomason@umail.ucsb.edu>
2016-12-04 13:28:02 -0800

    Changed how players spawn in net, may have problems

--- Assets/SpawnPillGuy.cs
+++ Assets/SpawnPillGuy.cs
@@ -4,12 +4,13 @@ using System.Collections;
 public class SpawnPillGuy : MonoBehaviour {
 
     public GameObject pillGuyObject;
+	public GameObject tablet;
     GameObject pillGuy;
 
     // Use this for initialization
     void Start () {
         pillGuy = Instantiate(pillGuyObject);
-        pillGuy.GetComponent<PillGuyMovement>().myTablet = this.transform.gameObject;
+		pillGuy.GetComponent<PillGuyMovement>().myTablet = tablet.transform.gameObject;
 	}
 	
 	// Update is called once per frame
