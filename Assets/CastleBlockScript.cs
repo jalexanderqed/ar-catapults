586bab1edb1a7da6d46035b0b7c6e5c33cf119f0
Jahwffrey <johnthomason@umail.ucsb.edu>
2016-12-06 11:53:09 -0800

    castle crumble should actually work now, also now its teapots

--- Assets/CastleBlockScript.cs
+++ Assets/CastleBlockScript.cs
@@ -4,9 +4,15 @@ using System.Collections;
 // Responsible for syncing castle blocks over the network
 public class CastleBlockScript : MonoBehaviour {
 
+	private bool destroyed = false;
+
 	void Update () {
-		if (transform.GetChild(0).position.y < -2)
-			Destroy (gameObject);
+		if (transform.GetChild (0).position.y < -2) {
+			if (!destroyed) {
+				destroyed = true;
+				transform.parent.GetComponent<CastleScript> ().childCrumbled ();
+			}
+		}
 	}
 }
 
